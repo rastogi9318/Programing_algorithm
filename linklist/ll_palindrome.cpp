f2aab73f7c5220ae78c913302d8201cf149bd787
Yash Rastogi <70816945+rastogi9318@users.noreply.github.com>
2022-10-08 20:57:30 +0530

    Create ll_palindrome.cpp

---
+++ linklist/ll_palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> st_ll;
+        ListNode *temp=head;
+        while(temp!=NULL)
+        {
+            st_ll.push(temp->val);
+            temp = temp->next;
+        }
+        while(head!=NULL)
+        {
+            int data = st_ll.top();
+            if(head->val!=data)
+            {
+                return false;
+            }
+            head=head->next;
+            st_ll.pop();
+        }
+        return true;
+        
+    }
+};
