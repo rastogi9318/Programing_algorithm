6def85caae4192e2dd3a11803e4410c9555ddd1d
Yash Rastogi <70816945+rastogi9318@users.noreply.github.com>
2022-10-10 00:15:03 +0530

    Update Intersection_point_linkedlist.cpp

--- gfg/Intersection_point_linkedlist.cpp
+++ gfg/Intersection_point_linkedlist.cpp
@@ -1,4 +1,7 @@
-
+/* Approach1
+Space:- O(1)
+Time:- O(m+n)
+*/
 int intersectPoint(Node* head1, Node* head2)
 {
     // Your Code Here
@@ -25,4 +28,70 @@ int intersectPoint(Node* head1, Node* head2)
     }
     return temp1->data;
 }
+/*
+Approach 2
+Space:- O(1)
+Time:- O(m+n)
+*/
+
+int length(Node *head)
+    {
+        int cnt = 0;
+        while(head != NULL)
+        {
+            cnt++;
+            head = head->next;
+        }
+        return cnt;
+    }
+
+int intersectPoint(Node *head1, Node *head2)
+ {
+         // code here
+         Node *temp1 = head1;
+         Node *temp2 = head2;
+         int cnt1 = length(temp1);
+         int cnt2 = length(temp2);
+         if(cnt1 > cnt2)
+         {
+             int diff = cnt1-cnt2;
+             temp1 = head1;
+             while(diff != 0)
+             {
+                 temp1 = temp1->next;
+                 diff--;
+             }
+             temp2 = head2;
+             while(temp1 != NULL && temp2 != NULL)
+             {
+                 if(temp1 == temp2)
+                 {
+                     return temp1->data;
+                 }
+                 temp1 = temp1->next;
+                 temp2 = temp2->next;
+             }
+         }
+         else
+         {
+            int dif = cnt2-cnt1;
+            temp2 = head2;
+            while(dif != 0)
+            {
+                temp2 = temp2->next;
+                dif--;
+            }
+            temp1 = head1;
+            while(temp1 != NULL && temp2 != NULL)
+             {
+                 if(temp1 == temp2)
+                 {
+                     return temp1->data;
+                 }
+                 temp1 = temp1->next;
+                 temp2 = temp2->next;
+             }
+         }
+         return -1;
+ }
 
