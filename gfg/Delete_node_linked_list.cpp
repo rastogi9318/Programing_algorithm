78ca277888155f0e9e9ac98e6124a4439890b7a0
Yash Rastogi <70816945+rastogi9318@users.noreply.github.com>
2022-10-13 22:12:10 +0530

    Update Delete_node_linked_list.cpp

--- gfg/Delete_node_linked_list.cpp
+++ gfg/Delete_node_linked_list.cpp
@@ -1,3 +1,13 @@
+/*
+Input: 1 -> 3 -> 4 
+       x = 3
+Output: 1 -> 3
+Explanation:
+After deleting the node at 3rd
+position (1-base indexing), the
+linked list is as 1 -> 3. 
+*/
+
 Node* deleteNode(Node *head,int x)
 {
     //Your code here
